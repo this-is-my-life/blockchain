8e25012e9f1fc24f87fa40126694b03f799a5693
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2021-04-12 09:18:10 +0900

    feat: cli support

--- main.go
+++ main.go
@@ -2,31 +2,77 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	blockchain "github.com/pmh-only/blockchain/src"
 )
 
 func main() {
-	// chain := blockchain.CreateChain()
+	DATA_DIRECTORY := os.Getenv("DATA_DIRECTORY")
+	if len(DATA_DIRECTORY) < 1 {
+		DATA_DIRECTORY = "./data"
+	}
 
-	// chain.AddStringBlock("Wa Sans")
-	// chain.AddStringBlock("This is pmh-only/blockchain")
+	args := os.Args[1:]
+	if len(args) < 1 {
+		println("Invalid Subcommnd\n")
+		return
+	}
 
-	// lastBlock := chain.GetLatestBlock()
+	subcmd := args[0]
 
-	// fmt.Printf("nonce: %x\n", lastBlock.Head.Nonce)
-	// fmt.Printf("serial: %x\n", lastBlock.SerializationWithTail())
+	chain := blockchain.Load(DATA_DIRECTORY)
 
-	// serial := lastBlock.SerializationWithTail()
-	// cloneBlock := blockchain.Deserialization(serial)
+	fmt.Printf("Chain Valid: %v\n", chain.IsValid())
+	fmt.Printf("Chain Length: %d blocks\n", len(chain.Blocks))
 
-	// fmt.Printf("is same? %v\n", string(cloneBlock.SerializationWithTail()) == string(serial))
-	// fmt.Printf("is valid? %v %v", lastBlock.IsValid(), cloneBlock.IsValid())
+	fmt.Println()
 
-	chain := blockchain.Load("./data")
+	if subcmd == "list" {
+		for _, block := range chain.Blocks {
+			fmt.Printf("%d. (%d) %s\n", block.Head.Index, block.Body.Flag, block.Body.Message)
+		}
 
-	fmt.Printf("%v\n", chain.GetLatestBlock().SerializationWithTail())
-	fmt.Printf("%v %v", len(chain.Blocks), chain.IsValid())
+		return
+	}
 
-	chain.Save("./data")
+	if subcmd == "list-detail" {
+		for _, block := range chain.Blocks {
+			fmt.Printf("\nIndex: %d\nCreatedAt: %d\nPrevHash: %x\nNonce: %d\nDifficulty: %d\nBody: (%d) %s\nCurrHash: %x\n",
+				block.Head.Index,
+				block.Head.CreatedAt,
+				block.Head.PrevHash,
+				block.Head.Nonce,
+				block.Head.Difficulty,
+				block.Body.Flag,
+				block.Body.Message,
+				block.Tail.CurrHash,
+			)
+		}
+
+		return
+	}
+
+	if subcmd == "add" {
+		if len(args) < 2 {
+			println("Uncompleted Subcommand")
+			return
+		}
+
+		data := strings.Join(args[1:], " ")
+		chain.AddStringBlock(data)
+		chain.Save(DATA_DIRECTORY)
+
+		lastCreated := chain.GetLatestBlock()
+
+		fmt.Printf("Block Created:\n%x", lastCreated.SerializationWithTail())
+		return
+	}
+
+	if subcmd == "drop" {
+		chain.Drop(DATA_DIRECTORY)
+
+		println("Dropped")
+	}
 }
